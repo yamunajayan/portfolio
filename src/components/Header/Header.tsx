934fb88908bb647dd19351c9848247526da62e9a
yamuna <yamunajayan@gmail.com>
2024-11-18 20:04:51 -0500

    projects page update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="md:flex justify-between font-custom p-4 md:p-8 md:max-w-5xl lg:mx-auto">
       <div className="text-gray-300 flex justify-center font-custom py-4 md:py-0">
-        <h3>YAMUNA JAYAN</h3>
+        <Link to={"/"} className="hover:cursor-pointer">
+          <h3>YAMUNA JAYAN</h3>
+        </Link>
       </div>
       <nav className="md:w-1/2">
         <ul className="text-gray-300 flex justify-between text-xs ">
-          <li className="text-yellow-600">Home</li>
-          <li>Projects</li>
+          <Link to={"/"} className="hover:cursor-pointer">
+            <li className="text-yellow-600">Home</li>
+          </Link>
+          <a href="#projects">
+            <li>Projects</li>
+          </a>
           <li>Education</li>
           <li>Experience</li>
         </ul>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,11 +4,14 @@ import projectData from "../../data/projects.json";
 const Projects = () => {
   console.log(projectData);
   return (
-    <section className="md:flex flex-col justify-center font-custom p-4 md:p-8 max-w-5xl lg:mx-auto text-gray-300">
+    <section
+      className="md:flex flex-col justify-center font-custom p-4 md:p-8 max-w-5xl lg:mx-auto text-gray-300"
+      id="projects"
+    >
       <div className="flex justify-center p-4">
         <h2 className="text-4xl md:text-5xl">PROJECTS</h2>
       </div>
-      <div className="flex flex-wrap gap-2 justify-center">
+      <div className="md:flex flex-wrap gap-3 justify-center">
         {projectData.map((project) => {
           return <ProjectCard key={project.id} project={project} />;
         })}
